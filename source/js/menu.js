653701329c4ffe41961c7de9272f3c7512a5e823
Alexander Tikhonov <at1385@yandex.ru>
2020-04-27 00:54:21 +0500

    Оживляет меню всех версий на всех стрницах

---
+++ source/js/menu.js
@@ -0,0 +1,10 @@
+var pageHeader = document.querySelector(".page-header");
+var menu = pageHeader.querySelector(".menu");
+var menuToggle = menu.querySelector(".menu__toggle");
+
+pageHeader.classList.toggle("page-header--js");
+menu.classList.toggle("menu--closed");
+
+menuToggle.addEventListener("click", function () {
+  menu.classList.toggle("menu--closed");
+});
